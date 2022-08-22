40a49b6bf51b49d69feb292dc3433896c607be7c
Andrew Buvria <buvrya228@gmail.com>
2022-08-22 23:38:00 +0300

    auth guard and some changes

--- core/views.py
+++ core/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 def loginView(request):
-    return render(request, 'login.html', {})
+    context = {}
+    if request.POST: 
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            #return redirect('/main')
+        else:
+            context['auth'] = False
+    return render(request, 'login.html', context)
 
 def signupView(request):
     return render(request, 'signup.html', {})
+
+@login_required(login_url='/login/')
+def mainView(request):
+    return render(request, 'main.html', {})
--- logsystem/urls.py
+++ logsystem/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from core.views import loginView, signupView
+from core.views import loginView, signupView, mainView
 
 urlpatterns = [
-    path('login', loginView, name='login'),
-    path('signup', signupView, name='signup'),
+    path('login/', loginView, name='login'),
+    path('signup/', signupView, name='signup'),
+    path('main/', mainView, name='main'),
     path('admin/', admin.site.urls),
 ]
